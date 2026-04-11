76e541b622fdeb5daec55a079d52402ae240b4b3
jason13official <jason13official@gmail.com>
2026-04-11 17:08:52 -0400

    client entrypoints

--- forge/src/main/java/io/github/jason13official/examplemod/ExampleModForge.java
+++ forge/src/main/java/io/github/jason13official/examplemod/ExampleModForge.java
@@ -1,19 +1,31 @@
 package io.github.jason13official.examplemod;
 
+import java.util.function.Consumer;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLLoader;
 
 @Mod(Constants.MOD_ID)
 public class ExampleModForge {
 
+  public static IEventBus EVENT_BUS;
+
+  public ExampleModForge(final FMLJavaModLoadingContext context) {
+    EVENT_BUS = context.getModEventBus();
+
+    EVENT_BUS.addListener((Consumer<FMLCommonSetupEvent>) event -> ExampleMod.init());
+    
+    if (FMLLoader.getDist() == Dist.CLIENT) {
+      new ExampleModClientForge(EVENT_BUS);
+    }
+  }
+
+  @Deprecated
+  @SuppressWarnings("all")
   public ExampleModForge() {
-
-    // This method is invoked by the Forge mod loader when it is ready
-    // to load your mod. You can access Forge and Common code in this
-    // project.
-
-    // Use Forge to bootstrap the Common mod.
-    Constants.LOG.info("Hello Forge world!");
-    ExampleMod.init();
-
+    this(FMLJavaModLoadingContext.get());
   }
 }
