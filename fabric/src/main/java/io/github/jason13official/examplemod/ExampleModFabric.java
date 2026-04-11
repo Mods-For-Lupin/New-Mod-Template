74285df05587aad865f3dfe5995bdcf959044dbf
jason13official <jason13official@gmail.com>
2026-04-11 19:20:00 -0400

    setup game object binding classes

--- fabric/src/main/java/io/github/jason13official/examplemod/ExampleModFabric.java
+++ fabric/src/main/java/io/github/jason13official/examplemod/ExampleModFabric.java
@@ -1,18 +1,37 @@
 package io.github.jason13official.examplemod;
 
+import io.github.jason13official.examplemod.impl.common.registry.ModBlocks;
+import io.github.jason13official.examplemod.impl.common.registry.ModEntities;
+import io.github.jason13official.examplemod.impl.common.registry.ModItems;
+import io.github.jason13official.examplemod.impl.common.registry.ModMenus;
+import io.github.jason13official.examplemod.impl.common.registry.ModParticles;
+import io.github.jason13official.examplemod.impl.common.registry.ModTabs;
+import io.github.jason13official.examplemod.impl.common.registry.ModTiles;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
 
 public class ExampleModFabric implements ModInitializer {
 
   @Override
   public void onInitialize() {
 
-    // This method is invoked by the Fabric mod loader when it is ready
-    // to load your mod. You can access Fabric and Common code in this
-    // project.
+    bind(BuiltInRegistries.BLOCK, ModBlocks::register);
+    bind(BuiltInRegistries.ENTITY_TYPE, ModEntities::register);
+    bind(BuiltInRegistries.ITEM, ModItems::register);
+    bind(BuiltInRegistries.PARTICLE_TYPE, ModParticles::register);
+    bind(BuiltInRegistries.BLOCK_ENTITY_TYPE, ModTiles::register);
+    bind(BuiltInRegistries.MENU, ModMenus::register);
+    bind(BuiltInRegistries.CREATIVE_MODE_TAB, ModTabs::register);
 
-    // Use Fabric to bootstrap the Common mod.
-    Constants.LOG.info("Hello Fabric world!");
     ExampleMod.init();
+  }
+
+  public <T> void bind(Registry<T> registry, Consumer<BiConsumer<T, ResourceLocation>> source) {
+
+    source.accept((t, rl) -> Registry.register(registry, rl, t));
   }
 }
--- forge/src/main/java/io/github/jason13official/examplemod/ExampleModForge.java
+++ forge/src/main/java/io/github/jason13official/examplemod/ExampleModForge.java
@@ -1,12 +1,26 @@
 package io.github.jason13official.examplemod;
 
+import io.github.jason13official.examplemod.impl.common.registry.ModBlocks;
+import io.github.jason13official.examplemod.impl.common.registry.ModEntities;
+import io.github.jason13official.examplemod.impl.common.registry.ModItems;
+import io.github.jason13official.examplemod.impl.common.registry.ModMenus;
+import io.github.jason13official.examplemod.impl.common.registry.ModParticles;
+import io.github.jason13official.examplemod.impl.common.registry.ModTabs;
+import io.github.jason13official.examplemod.impl.common.registry.ModTiles;
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLLoader;
+import net.minecraftforge.registries.RegisterEvent;
 
 @Mod(Constants.MOD_ID)
 public class ExampleModForge {
@@ -15,6 +29,14 @@ public class ExampleModForge {
 
   public ExampleModForge(final FMLJavaModLoadingContext context) {
     EVENT_BUS = context.getModEventBus();
+
+    bind(Registries.BLOCK, ModBlocks::register);
+    bind(Registries.ENTITY_TYPE, ModEntities::register);
+    bind(Registries.ITEM, ModItems::register);
+    bind(Registries.PARTICLE_TYPE, ModParticles::register);
+    bind(Registries.BLOCK_ENTITY_TYPE, ModTiles::register);
+    bind(Registries.MENU, ModMenus::register);
+    bind(Registries.CREATIVE_MODE_TAB, ModTabs::register);
 
     EVENT_BUS.addListener((Consumer<FMLCommonSetupEvent>) event -> ExampleMod.init());
 
@@ -27,5 +49,14 @@ public class ExampleModForge {
   @SuppressWarnings("all")
   public ExampleModForge() {
     this(FMLJavaModLoadingContext.get());
+  }
+
+  public <T> void bind(ResourceKey<Registry<T>> registryKey, Consumer<BiConsumer<T, ResourceLocation>> source) {
+
+    EVENT_BUS.addListener((Consumer<RegisterEvent>) event -> {
+      if (registryKey.equals(event.getRegistryKey())) {
+        source.accept((t, rl) -> event.register(registryKey, rl, () -> t));
+      }
+    });
   }
 }
