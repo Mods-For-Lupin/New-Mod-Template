6954bf9297011580c58423d66beff97a2c969631
jason13official <jason13official@gmail.com>
2026-04-11 19:23:50 -0400

    spawn egg item helper

--- forge/src/main/java/io/github/jason13official/examplemod/platform/ForgePlatformHelper.java
+++ forge/src/main/java/io/github/jason13official/examplemod/platform/ForgePlatformHelper.java
@@ -2,8 +2,14 @@ package io.github.jason13official.examplemod.platform;
 
 import io.github.jason13official.examplemod.platform.services.IPlatformHelper;
 import java.nio.file.Path;
+import java.util.function.Supplier;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Mob;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTab.Builder;
+import net.minecraft.world.item.Item.Properties;
+import net.minecraft.world.item.SpawnEggItem;
+import net.minecraftforge.common.ForgeSpawnEggItem;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.loading.FMLLoader;
 import net.minecraftforge.fml.loading.FMLPaths;
@@ -38,5 +44,11 @@ public class ForgePlatformHelper implements IPlatformHelper {
   public Builder tabBuilder() {
 
     return CreativeModeTab.builder();
+  }
+
+  @Override
+  public SpawnEggItem createSpawnEggItem(Supplier<EntityType<? extends Mob>> typeSupplier, int background, int highlight, Properties properties) {
+    
+    return new ForgeSpawnEggItem(typeSupplier, background, highlight, properties);
   }
 }
