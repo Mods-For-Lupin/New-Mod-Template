755e5d1a4c180638de9ae9f0097cc63201752ad6
jason13official <jason13official@gmail.com>
2026-04-11 20:26:01 -0400

    identifier registration fix

--- common/src/main/java/io/github/jason13official/examplemod/impl/common/registry/ModParticles.java
+++ common/src/main/java/io/github/jason13official/examplemod/impl/common/registry/ModParticles.java
@@ -2,10 +2,10 @@ package io.github.jason13official.examplemod.impl.common.registry;
 
 import java.util.function.BiConsumer;
 import net.minecraft.core.particles.ParticleType;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 
 public class ModParticles {
 
-  public static void register(BiConsumer<ParticleType<?>, ResourceLocation> consumer) {
+  public static void register(BiConsumer<ParticleType<?>, Identifier> consumer) {
   }
 }
