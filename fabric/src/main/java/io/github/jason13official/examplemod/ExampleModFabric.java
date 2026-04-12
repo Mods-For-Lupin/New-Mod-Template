8442c968c35eb25ddb1225eea7a48a20c3f1e642
jason13official <jason13official@gmail.com>
2026-04-11 21:18:59 -0400

    add resource reload listeners

--- fabric/src/main/java/io/github/jason13official/examplemod/ExampleModFabric.java
+++ fabric/src/main/java/io/github/jason13official/examplemod/ExampleModFabric.java
@@ -10,9 +10,19 @@ import io.github.jason13official.examplemod.impl.common.registry.ModTiles;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener;
+import net.fabricmc.fabric.impl.resource.DataResourceLoaderImpl;
+import net.fabricmc.fabric.impl.resource.FabricResourceReloader;
+import net.fabricmc.fabric.impl.resource.ResourceLoaderImpl;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.Identifier;
+import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.server.packs.resources.ResourceManagerReloadListener;
+import net.minecraft.server.packs.resources.SimplePreparableReloadListener;
+import net.minecraft.util.profiling.ProfilerFiller;
 
 public class ExampleModFabric implements ModInitializer {
 
@@ -28,10 +38,30 @@ public class ExampleModFabric implements ModInitializer {
     bind(BuiltInRegistries.CREATIVE_MODE_TAB, ModTabs::register);
 
     ExampleMod.init();
+
+    DataResourceLoaderImpl.get(PackType.SERVER_DATA).registerReloadListener(ExampleMod.identifier(Constants.MOD_ID), new ResourceReloadListener());
   }
 
   public <T> void bind(Registry<T> registry, Consumer<BiConsumer<T, Identifier>> source) {
 
     source.accept((t, rl) -> Registry.register(registry, rl, t));
+  }
+
+  public static class ResourceReloadListener extends SimplePreparableReloadListener<Void> {
+
+    @Override
+    public String getName() {
+      return ExampleMod.identifier(Constants.MOD_ID).toString();
+    }
+
+    @Override
+    protected void apply(Void unused, ResourceManager resourceManager, ProfilerFiller profilerFiller) {
+      // ModConfig.load(Services.PLATFORM.getConfigDirectory());
+    }
+
+    @Override
+    protected Void prepare(ResourceManager resourceManager, ProfilerFiller profilerFiller) {
+      return null;
+    }
   }
 }
